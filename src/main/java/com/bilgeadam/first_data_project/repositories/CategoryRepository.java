459e230be91d120b5a32512ffe7d08c07257aeb7
Veli Uysal (BilgeAdam Eğitmen) <veli.uysal@baogrenci.com>
2023-10-21 14:16:45 +0300

    JPA Methods & JPQL

--- src/main/java/com/bilgeadam/first_data_project/repositories/CategoryRepository.java
+++ src/main/java/com/bilgeadam/first_data_project/repositories/CategoryRepository.java
@@ -13,8 +13,8 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     List<Category> findByName(String name);
 
-    @Query("SELECT c FROM Category c WHERE c.description LIKE %:description%")
-    List<Category> findTurkishCategoriesInDescription(@Param("description") String description);
+    @Query("SELECT c FROM Category c WHERE c.description LIKE %:descr%")
+    List<Category> findTurkishCategoriesInDescription(@Param("descr") String description);
 
     @Query(value = "SELECT c.* FROM Categories c WHERE c.description LIKE %:description%", nativeQuery = true)
     List<Category> findTurkishCategoriesInDescriptionByNativeQuery(@Param("description") String description);
